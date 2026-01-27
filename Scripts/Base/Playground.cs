89b665c312e35e4a96e293c08833732dccafb31b
Akarsh Chauhan <akarshchauhan15@gmail.com>
2026-01-28 00:32:49 +0530

    Functional infinite level generation and fixed modular map components.

--- Scripts/Base/Playground.cs
+++ Scripts/Base/Playground.cs
@@ -3,18 +3,22 @@ using Godot;
 public partial class Playground : Node2D
 {
     Node2D LevelContainer;
+    PackedScene NextMapPackedComponent;
 
     public override void _Ready()
     {
         LevelContainer = GetNode<Node2D>("Levels");
+        NextMapPackedComponent = BaseMapDefaults.ModularLevelScenes[0];
     }
     public void SpawnModularLevelComponent(Vector2 SacrificedPosition)
     {
-        GD.Print((int)BaseMapDefaults.ModularLevelNamesEnum.Centre);
-        Node2D MapComponent = BaseMapDefaults.ModularLevelScenes[(int)BaseMapDefaults.ModularLevelNamesEnum.Centre].Instantiate<Node2D>();
+        BaseMapComponent MapComponent = NextMapPackedComponent.Instantiate<BaseMapComponent>();
         
         MapComponent.Position = new Vector2(0, SacrificedPosition.Y - 720 * 3);
         LevelContainer.AddChild(MapComponent);
+
+        BaseMapDefaults.ModularLevelNamesEnum NextMapEnum = MapComponent.NextModularLevels.PickRandom();
+        NextMapPackedComponent =  BaseMapDefaults.ModularLevelScenes[(int)NextMapEnum];
     }
 
 }

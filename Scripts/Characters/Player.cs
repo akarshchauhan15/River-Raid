a64f2a106edb31f47ad46ab721d4cc1717ce32aa
Akarsh Chauhan <akarshchauhan15@gmail.com>
2026-01-28 00:31:39 +0530

    Added camera effects and vignette overlay.

--- Scripts/Characters/Player.cs
+++ Scripts/Characters/Player.cs
@@ -3,18 +3,23 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-    float MaxVelocity = 700;
-    float MinVelocity = 300;
-    Vector2 Acceleration = new(2500, 500);
-    Vector2 Friction = new(1600, 2500);
+    Camera2D Camera;
+
+    Vector2 MaxVelocity = new(700, 400);
+    float MinVelocityY = 200;
+    Vector2 Acceleration = new(2500, 400);
+    Vector2 Friction = new(1600, 2400);
 
     public override void _Ready()
     {
+        Camera = GetNode<Camera2D>("../Camera");
+
         Velocity = new(0, 0);
     }
-    public override void _PhysicsProcess(double delta)
+    public override void _Process(double delta)
     {
         CheckMovement(delta);
+        AlignCamera();
     }
     public override void _Input(InputEvent @event)
     {
@@ -32,16 +37,21 @@ public partial class Player : CharacterBody2D
 
         //Horizontal Velocity
         if (Direction.X != 0)
-            Velocity = Velocity.MoveToward(new Vector2(Direction.X  * MaxVelocity, Velocity.Y), Acceleration.X *  (float) delta);
+            Velocity = Velocity.MoveToward(new Vector2(Direction.X  * MaxVelocity.X, Velocity.Y), Acceleration.X *  (float) delta);
         else
             Velocity = Velocity.MoveToward(new Vector2(0, Velocity.Y), Friction.X * (float) delta);
 
         if (Direction.Y < 0)
-            BaseMapComponent.Speed = Mathf.MoveToward(BaseMapComponent.Speed, MaxVelocity, Mathf.Abs(Direction.Y) * Acceleration.Y * (float) delta);
+            BaseMapComponent.Speed = Mathf.MoveToward(BaseMapComponent.Speed, MaxVelocity.Y, Mathf.Abs(Direction.Y) * Acceleration.Y * (float) delta);
         else if (Direction.Y > 0)
-            BaseMapComponent.Speed = Mathf.MoveToward(BaseMapComponent.Speed, MinVelocity, Mathf.Abs(Direction.Y) * Acceleration.Y * (float) delta);
+            BaseMapComponent.Speed = Mathf.MoveToward(BaseMapComponent.Speed, MinVelocityY, Mathf.Abs(Direction.Y) * Acceleration.Y * (float) delta);
 
         MoveAndSlide();
+    }
+    private void AlignCamera()
+    {
+        float NewPositionX = 640 + (GlobalPosition.X - 640) * 0.1f;
+        Camera.GlobalPosition = new(NewPositionX, Camera.GlobalPosition.Y);
     }
     private void Shoot()
     {

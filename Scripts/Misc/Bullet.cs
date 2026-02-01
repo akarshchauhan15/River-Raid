4361905d7078ed9f3dbca3c025762b018edd9ab8
Akarsh Chauhan <akarshchauhan15@gmail.com>
2026-02-01 22:30:03 +0530

    Added animation and updated timeout in bullet scene.

--- Scripts/Misc/Bullet.cs
+++ Scripts/Misc/Bullet.cs
@@ -8,13 +8,21 @@ public partial class Bullet : Area2D
 
     public override void _Ready()
     {
-        GetNode<Timer>("Timer").Timeout += QueueFree;
+        GetNode<Timer>("Timer").Timeout += Fall;
         BodyEntered += OnCollision;
     }
     public override void _Process(double delta)
     {
         Position += Direction * Speed * (float) delta;
         Speed -= 100 * (float) delta;
+    }
+    private void Fall()
+    {
+        Tween T = CreateTween();
+        Sprite2D S = GetNode<Sprite2D>("Sprite2D");
+
+        T.TweenProperty(S, Sprite2D.PropertyName.Scale.ToString(), Vector2.One * 0.1f, 1f).SetTrans(Tween.TransitionType.Cubic);
+        T.TweenCallback(Callable.From(QueueFree)).SetDelay(0.5f);
     }
     private void OnCollision(Node2D Body)
     {

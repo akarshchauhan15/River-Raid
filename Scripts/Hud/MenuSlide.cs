87652f516aaf62550b73b7baa0083259c10dfcd5
Akarsh Chauhan <akarshchauhan15@gmail.com>
2026-02-16 22:39:20 +0530

    Added fullscreen toggle button in menu slide.

--- Scripts/Hud/MenuSlide.cs
+++ Scripts/Hud/MenuSlide.cs
@@ -3,14 +3,27 @@ using System;
 
 public partial class MenuSlide : Slide
 {
+    TextureButton FullscreenButton;
     TextureButton PauseButton;
+
     public override void _Ready()
     {
         base._Ready();
 
+        FullscreenButton = GetNode<TextureButton>("ColorRect/FullscreenButton");
         PauseButton = GetNode<TextureButton>("ColorRect/PauseButton");
 
+        FullscreenButton.Toggled += OnFullscreenButtonToggled;
         PauseButton.Toggled += OnPauseButtonToggled;
+    }
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("Pause"))
+            OnPauseButtonToggled(true);
+    }
+    private void OnFullscreenButtonToggled(bool Fullscreen)
+    {
+        DisplayServer.WindowSetMode(Fullscreen ? DisplayServer.WindowMode.Fullscreen : DisplayServer.WindowMode.Windowed);
     }
     private void OnPauseButtonToggled(bool Paused)
     {

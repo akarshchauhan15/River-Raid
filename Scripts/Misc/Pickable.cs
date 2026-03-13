66824485d28cd21b67c11608a6ba5d9bfea1b786
Akarsh Chauhan <akarshchauhan15@gmail.com>
2026-03-14 01:30:43 +0530

    Added pickable items.

---
+++ Scripts/Misc/Pickable.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class Pickable : Area2D
+{
+    public enum PickableType { Fuel, Health, Shield }
+
+    public PickableType Type { get; set; }
+
+    public override void _Ready()
+    {
+        BodyEntered += OnPlayerEntered;
+        GetNode<Sprite2D>("Sprite2D").Frame = (int)Type;
+    }
+    private void OnPlayerEntered(Node2D Body)
+    {    
+        if (!(Body is Player)) return;
+        QueueFree();
+
+        Player Player = Body as Player;
+        Player.EmitSignal(Player.SignalName.EventHappened, (int)Type);
+
+        switch (Type) 
+        {
+            case PickableType.Fuel:
+                Player.Fuel = Math.Min(Player.Fuel + 40, 100);
+                return;
+
+            case PickableType.Health:
+                Player.Health = Math.Min(Player.Health + 1, 3);
+                return;
+
+        }
+
+        
+    }
+}
